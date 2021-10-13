ce51e78bd3d9b4bba33d89992a1966268f43c0e1
Layth Zahid <laith43d@gmail.com>
2021-10-14 00:08:07 +0300

    w2-l2-task2

---
+++ headless/utils.py
@@ -0,0 +1,41 @@
+import re
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+
+def list_posts():
+    """
+    Returns a list of all names of blog posts.
+    """
+    _, filenames = default_storage.listdir("posts")
+    return list(sorted(re.sub(r"\.md$", "", filename)
+                for filename in filenames if filename.endswith(".md")))
+
+
+def save_post(title, content):
+    """
+    Saves a blog post, given its title and Markdown
+    content. If an existing post with the same title already exists,
+    it is replaced.
+    """
+    filename = f"posts/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
+
+
+def get_post(title):
+    """
+    Retrieves a post by its title. If no such
+    post exists, the function returns None.
+    """
+    try:
+        f = default_storage.open(f"posts/{title}.md")
+        return f.read().decode("utf-8")
+    except FileNotFoundError:
+        return None
+
+
+def del_post(title):
+    pass
