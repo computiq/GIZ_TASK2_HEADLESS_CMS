8f76be92bb835766e9c41a6047a6c44182a2fa6e
hassan12ammar <mgamea125@gmail.com>
2021-10-15 05:30:31 +0300

    my Solution : )

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from headless.controllers import headless_controlear
+
+api = NinjaAPI()
+api.add_router("headless", headless_controlear)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", api.urls),
 ]
+
---
+++ posts/Test.md
@@ -0,0 +1,1 @@
+Just for Test, I'ts Working and Updated
