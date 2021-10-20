63f65fcb39e9ac80b87ba9badb08488a1368bc41
Mustafa Bin Hadi <91639104+mustafabinhadi@users.noreply.github.com>
2021-10-20 15:58:46 +0300

    Make it real
    Do all the requirements

--- headless/utils.py
+++ headless/utils.py
@@ -1,24 +1,16 @@
 import re
-
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
-
 def list_posts():
-    """
-    Returns a list of all names of blog posts.
-    """
+    "Returns list for all blog names"
     _, filenames = default_storage.listdir("posts")
     return list(sorted(re.sub(r"\.md$", "", filename)
                 for filename in filenames if filename.endswith(".md")))
 
 
 def save_post(title, content):
-    """
-    Saves a blog post, given its title and Markdown
-    content. If an existing post with the same title already exists,
-    it is replaced.
-    """
+    "This function saves a blog post based on its title and content.If a post with the identical title already exists, it will be replaced"
     filename = f"posts/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
@@ -26,16 +18,19 @@ def save_post(title, content):
 
 
 def get_post(title):
-    """
-    Retrieves a post by its title. If no such
-    post exists, the function returns None.
-    """
+    "Returns a post based on its title. If there is no such post, the function returns None"
     try:
         f = default_storage.open(f"posts/{title}.md")
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
 
-
 def del_post(title):
-    pass
+    " Removes a post based on its title. If there is no such post, the function returns None"
+    try:
+        path = (f"posts/{title}.md")
+        file_content = default_storage.open(path).read().decode("utf-8")
+        default_storage.delete(path)
+        return file_content
+    except FileNotFoundError:
+        return None
