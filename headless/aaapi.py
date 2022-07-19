60142f88d0a36144ddf17accb15b91af424407e0
OMER RAAD <omerraad444@gmail.com>
2022-07-19 17:07:46 +0300

    task 2

---
+++ headless/aaapi.py
@@ -0,0 +1,8 @@
+from ninja import NinjaAPI
+from ninja import Router
+from ninja import Schema
+
+
+test = Router()
+
+api = NinjaAPI()
---
+++ headless/controllers.py
@@ -0,0 +1,18 @@
+# from ninja import NinjaAPI
+# from ninja import Router
+# from ninja import Schema
+from headless.aaapi import api , test
+from headless.utils import list_posts
+from headless.utils import save_post
+from headless.utils import get_post
+from headless.utils import del_post
+from headless.utils import update_post
+
+
+
+# @test.get('/posts')
+list_posts('')
+save_post('', '', '')
+get_post('','')
+update_post('', '', '')
+del_post('', '')
