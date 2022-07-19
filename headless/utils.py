9f06f68cfd12f1e2f3e901e463948c6cf0b24b9a
devsadeq <dev.sadeq@gmail.com>
2022-07-19 15:31:25 +0300

    initial commit 🚀

--- headless/utils.py
+++ headless/utils.py
@@ -10,7 +10,7 @@ def list_posts():
     """
     _, filenames = default_storage.listdir("posts")
     return list(sorted(re.sub(r"\.md$", "", filename)
-                for filename in filenames if filename.endswith(".md")))
+                       for filename in filenames if filename.endswith(".md")))
 
 
 def save_post(title, content):
