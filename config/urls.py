e000d26481d3add70acbd649744dc51dcdac947a
Al-Qasim Mahmood <104140247+KasmThio@users.noreply.github.com>
2022-07-19 16:51:21 +0300

    solved

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from headless.controllers import CMS_controller
+
+api = NinjaAPI()
+api.add_router('', CMS_controller)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
+
 ]
---
+++ headless/controllers.py
@@ -0,0 +1,71 @@
+from ninja import Router
+from headless.utils import *
+
+CMS_controller = Router()
+
+
+@CMS_controller.get('posts')
+def list_posts(request):
+    """
+    Returns a list of all names of blog posts.
+    """
+    _, filenames = default_storage.listdir("posts")
+    return list(sorted(re.sub(r"\.md$", "", filename)
+                       for filename in filenames if filename.endswith(".md")))
+
+
+@CMS_controller.get('posts/title')
+def get_post(request, title):
+    """
+    Retrieves a post by its title. If no such
+    post exists, the function returns None.
+    """
+    try:
+        f = default_storage.open(f"posts/{title}.md")
+        return f.read().decode("utf-8")
+    except FileNotFoundError:
+        return None
+
+
+@CMS_controller.post('create')
+def save_post(request, title, content):
+    """
+    Saves a blog post, given its title and Markdown
+    content. If an existing post with the same title already exists,
+    it is replaced.
+    """
+    filename = f"posts/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
+
+
+@CMS_controller.put('update')
+def save_post(request, title, content):
+    """
+    Saves a blog post, given its title and Markdown
+    content. If an existing post with the same title already exists,
+    it is replaced.
+    """
+    filename = f"posts/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
+
+
+@CMS_controller.delete('delete')
+def del_post(request, title):
+    """
+    Delete a blog post, given its title.
+    """
+    filename = f"posts/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+        default_storage.delete(ContentFile())
+
+
+@CMS_controller.get('add')
+def add(request, a: int, b: int):
+    x = a + b * 2
+    return x
+
