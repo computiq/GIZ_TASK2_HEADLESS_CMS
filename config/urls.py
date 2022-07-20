59a6b313888d65cc210ad31ba47ecd190b84a111
devsadeq <dev.sadeq@gmail.com>
2022-07-20 16:10:57 +0300

    create api v1 ✔

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from config.api.v1.api import api as api_v1
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', api_v1.urls),
 ]
