f5a6d95c7b446e39ca24c7e924c29609395258b3
devsadeq <dev.sadeq@gmail.com>
2022-07-20 16:08:30 +0300

    edit the post model ✔

--- headless/models.py
+++ headless/models.py
@@ -1,11 +1,10 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
-from config.models import BaseModel
 
-
-class Post(BaseModel):
+class Post(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
-    content = models.TextField(null=False, blank=False)
+    content = RichTextField(null=False, blank=False)
 
     def __str__(self):
         return self.title
