854130e9e9faeb7f38df945d0a9ac88febefd517
Mohammed <mmmmxc7@gmail.com>
2022-07-20 13:22:25 +0300

    task after solution

--- headless/utils.py
+++ headless/utils.py
@@ -4,7 +4,7 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
 #DONE
-@test.get('/posts')
+@api.get('/posts')
 def list_posts(request):
     """
     Returns a list of all names of blog posts.
@@ -13,7 +13,7 @@ def list_posts(request):
     return list(sorted(re.sub(r"\.md$", "", filename)
                 for filename in filenames if filename.endswith(".md")))
 #DONE
-@test.post('/path')
+@api.post('/path')
 def save_post(request, title, content):
     """
     Saves a blog post, given its title and Markdown
@@ -26,7 +26,7 @@ def save_post(request, title, content):
     default_storage.save(filename, ContentFile(content))
 
 #DONE
-@test.get('/posts/{title}')
+@api.get('/posts/{title}')
 def get_post(request, title):
     """
     Retrieves a post by its title. If no such
@@ -38,14 +38,14 @@ def get_post(request, title):
     except FileNotFoundError:
         return None
 
-@test.delete('/posts/{title}')
+@api.delete('/posts/{title}')
 def del_post(request, title):
     filename = f"posts/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
         
 
-@test.put('/path/{title}')
+@api.put('/path/{title}')
 def update_post(request, title, content):
     """
     update the content of post by given it's title
