628b48ae69c5551581069682eaff768a5251ae09
devsadeq <dev.sadeq@gmail.com>
2022-07-20 16:08:55 +0300

    edit the base model ✔

--- config/models.py
+++ config/models.py
@@ -2,6 +2,10 @@ import uuid as uuid
 
 from django.db import models
 
+"""
+As we not using database in  this task, this base model will not be used yet.
+"""
+
 
 class BaseModel(models.Model):
     id = models.IntegerField(primary_key=True, blank=False, null=False, auto_created=True, editable=False)
