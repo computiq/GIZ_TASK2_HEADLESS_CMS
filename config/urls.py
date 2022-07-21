cc4da251a76d1f77e640a72215a251d3291f51f3
ahmedlord20 <103456191+ahmedlord20@users.noreply.github.com>
2022-07-21 18:04:04 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/' , api.urls),
 ]
